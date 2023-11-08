ac80fa4641e353fb93787ca418b26308870f60f6
Tim Schreiner <tim.schreiner@km2.de>
2023-11-08 15:56:58 +0100

    [RELEASE] Raise version to 0.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,12 +8,12 @@ $EM_CONF[$_EXTKEY] = [
     'author_email' => 'tim.schreiner@km2.de',
     'author_company' => 'FGTCLB',
     'state' => 'beta',
-    'version' => '0.1.0',
+    'version' => '0.2.0',
     'clearCacheOnLoad' => true,
     'constraints' => [
         'depends' => [
             'typo3' => '11.5.0-11.5.99',
-            'academic_persons' => '0.1.0 - 0.1.99',
+            'academic_persons' => '0.2.0 - 0.2.99',
         ],
         'conflicts' => [
         ],
