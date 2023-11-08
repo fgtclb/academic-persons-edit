fd0c8cceb847936ff462aaca81fa4b8b0912b411
Tim Schreiner <tim.schreiner@km2.de>
2023-11-08 15:37:01 +0100

    [TASK] Allow editing profile informations

--- Resources/Public/JavaScript/FrontendEditor.js
+++ Resources/Public/JavaScript/FrontendEditor.js
@@ -17,18 +17,32 @@
     ]
   };
 
+  var editorConfigLinkOnly = {
+    language: 'en',
+    height: 350,
+    format_tags: 'p',
+    toolbarGroups: [
+      { name: 'links' },
+      { name: 'clipboard', groups: [ 'cleanup' ] }
+    ],
+    customConfig: '',
+    removeButtons: [
+      'Anchor',
+    ]
+  };
+
   var waitCKEDITOR = setInterval(function() {
     console.log('Wait for ckeditor');
     if (window.CKEDITOR) {
       clearInterval(waitCKEDITOR);
       CKEDITOR.replace('profile-teaching-area', editorConfig);
       CKEDITOR.replace('profile-core-competences', editorConfig);
-      CKEDITOR.replace('profile-memberships', editorConfig);
       CKEDITOR.replace('profile-supervised-thesis', editorConfig);
       CKEDITOR.replace('profile-supervised-doctoral-thesis', editorConfig);
-      CKEDITOR.replace('profile-vita', editorConfig);
-      CKEDITOR.replace('profile-publications', editorConfig);
       CKEDITOR.replace('profile-miscellaneous', editorConfig);
+      document.querySelectorAll('.publication-bodytext').forEach((textarea) => {
+        CKEDITOR.replace(textarea, editorConfigLinkOnly);
+      });
     }
   }, 100);
 })();
