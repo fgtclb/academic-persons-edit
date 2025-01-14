47f4eb523cf4c8d9dd40d52c96346b21055d58f2
Philipp Halle <p.halle@web-vision.de>
2025-01-14 16:06:58 +0100

    [TASK] Remove profile switcher and add granular actions

--- Configuration/TCA/Overrides/tt_content_plugins.php
+++ Configuration/TCA/Overrides/tt_content_plugins.php
@@ -13,18 +13,6 @@ use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
 
 ExtensionUtility::registerPlugin(
     'AcademicPersonsEdit',
-    'ProfileSwitcher',
-    'LLL:EXT:academic_persons_edit/Resources/Private/Language/locallang_be.xlf:plugin.profile_switcher.label',
-    'EXT:academic_persons_edit/Resources/Public/Icons/persons_edit_icon.svg'
-);
-$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['academicpersonsedit_profileswitcher'] = implode(',', [
-    'pages',
-    'recursive',
-    'select_key',
-]);
-
-ExtensionUtility::registerPlugin(
-    'AcademicPersonsEdit',
     'ProfileEditing',
     'LLL:EXT:academic_persons_edit/Resources/Private/Language/locallang_be.xlf:plugin.profile_editing.label',
     'EXT:academic_persons_edit/Resources/Public/Icons/persons_edit_icon.svg'
